81b00b12b01286b6869fae52798a1343d62e949e
erikgilbertsson <egilbertsson@gmail.com>
2021-02-24 22:19:04 +0100

    added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
-import { BrowserAnimationsModule } from
-  '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
 //IMPORTS
 import { MainComponent } from './main/main.component';
 import { FormComponent } from './form/form.component';
