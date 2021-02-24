9dc528c2c428e30ae384ffbae75b3abbbffcf24f
Samuel Alberius <samuel@alberius.se>
2021-02-24 13:07:20 +0100

    API key now gets token and can fetch

--- src/app/api-fetch.service.ts
+++ src/app/api-fetch.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { map } from 'rxjs/operators';
 
+import { OptionsService } from './options.service'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,7 +21,7 @@ export class ApiFetchService {
 
   constructor(private http: HttpClient) { }
 
-  generateToken() {
+  async generateToken() {
     //Spotify API requires Headers: Authorization, Content-Type for token request
     let headers = {
       'Authorization': 'Basic ' + btoa(this.client_id + ":" + this.client_secret),
@@ -31,23 +33,60 @@ export class ApiFetchService {
     let body = params.toString()
     //Calls angular HTTP POST with url, headers and data from above
     return this.http.post('https://accounts.spotify.com/api/token', body, { headers: headers })
-      .toPromise().then(res => JSON.stringify(res))
+      .toPromise();
   }
 
-  printToken() {
-    console.log(this.token)
-  }
-
-  generateGenre(token: string) {
+  async generateGenre(token: any) {
     //Spotify API requires Headers: Authorization, Content-Type and Accept for genre-request
     let headers = {
       'Authorization': 'Bearer ' + token,
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Accept': 'application/json'
+      'Accept': 'application/json',
     }
     //Calls angular HTTP GET with url and headers from above
-    this.http.get(this.query_url + 'recommendations/available-genre-seeds', { headers: headers })
+    return this.http.get(this.query_url + 'recommendations/available-genre-seeds', { headers: headers })
       .toPromise()
-      .then(res => console.log(res))
+  }
+
+  async generateArtist(token: any) {
+    //Spotify API requires Headers: Authorization, Content-Type and Accept for genre-request
+    let headers = {
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json',
+    }
+    //Calls angular HTTP GET with url and headers from above
+    return this.http.get(this.query_url + 'artists/3eqjTLE0HfPfh78zjh6TqT?si=T7SHFYmJQNyihg0towMHQQ', { headers: headers })
+      .toPromise()
+  }
+
+  async generateSong(token: any) {
+    //Spotify API requires Headers: Authorization, Content-Type and Accept for genre-request
+    let headers = {
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json',
+    }
+    //Calls angular HTTP GET with url and headers from above
+    return this.http.get(this.query_url + 'tracks/7lPN2DXiMsVn7XUKtOW1CS?si=fbTOTcZcRh291DRWNC3aeQ', { headers: headers })
+      .toPromise()
+  }
+
+  async generateRecommendation(token: any, seedArtist: string, seedGenre: string, seedSong: string, limit: string, popularity: string) {
+    let url = 'reccomendations?'
+      + 'seed_artist=' + seedArtist
+      + '&seed_genre=' + seedArtist
+      + '&seed_song=' + seedSong
+      + '&limit=' + limit
+      + '&popularity=' + popularity
+
+    let headers = {
+      'Authorization': 'Bearer ' + token,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json',
+    }
+    //Calls angular HTTP GET with url and headers from above
+    return this.http.get(this.query_url + url, { headers: headers })
+      .toPromise()
   }
 }
